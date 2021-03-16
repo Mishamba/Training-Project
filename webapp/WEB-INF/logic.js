3b2f2edb92ac2b5f392d2ac744e34b4c3f44e22a
Mishamba <misha.nenahov@gmail.com>
2021-03-16 20:46:39 +0300

    Added delete and show files funcitons for client Changed some css.

--- webapp/WEB-INF/logic.js
+++ webapp/WEB-INF/logic.js
@@ -24,9 +24,14 @@ menu.addEventListener('click', event => {
 let jwtToken = undefined;
 let username = 'guest';
 let role = 'guest';
-let serviceEndpoint = 'http://localhost:8081/';
 
-let currentEndpoint = '/home/mishamba';
+let serviceEndpoint = 'http://localhost:8081';
+let loginEndpoint = '/login';
+let createFileEndpoint = '/create-file';
+let deleteFileEndpoint = '/delete-file';
+
+let currentPath = '/home/mishamba';
+let currentFiles = {};
 
 const signInButton = document.querySelector('.sign-in-button');
 signInButton.addEventListener('click', signInProcessor());
@@ -35,7 +40,7 @@ async function signInProcessor() {
     let username = document.querySelector('#username-input');
     let password = document.querySelector('#password-input');
 
-    let responce = await fetch(serviceEndpoint + 'login', {
+    let response = await fetch(serviceEndpoint + loginEndpoint, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -43,11 +48,13 @@ async function signInProcessor() {
         body: JSON.stringify({ username: username, password: password })
     });
 
-    let jsonResponce = await responce.json();
+    let jsonResponse = await response.json();
 
-    jwtToken = jsonResponce['jwt'];
-    username = jsonResponce['username'];
-    role = jsonResponce['role'];
+    jwtToken = jsonResponse['jwt'];
+    username = jsonResponse['username'];
+    role = jsonResponse['role'];
+
+    signInButton.classList.remove('active');
 }
 
 let deleteFileElement = document.createAttribute('li');
@@ -63,11 +70,13 @@ function configurePageFunctions() {
         // Appending delete file option to context menu.
         if (deleteFileOption === null) {
             menu.append(deleteFileElement);
+            deleteFileElement.addEventListener('click', deleteFile());
         }
 
         // Making create file button visible.
-        if (activeCreateFileButton === null) {
+        if (createFileButton === null) {
             createFileButton.classList.add('active');
+            activeCreateFileButton = createFileButton;
         }
     } else {
         // Removing delete file option from context menu.
@@ -77,7 +86,141 @@ function configurePageFunctions() {
 
         // Making create file button non-visible.
         if (activeCreateFileButton !== null) {
-            activeCreateFileButton.parentElement.remove(activeCreateFileButton);
+            activeCreateFileButton.classList.remove('active');
+            createFileButton = activeCreateFileButton;
         }
+    }
+}
+
+async function deleteFile() {
+    let fileName = findFileName();
+
+    let response = await fetch(serviceEndpoint + deleteFileEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ fileName: fileName, path: currentPath })
+    });
+
+    if (response.ok) {
+        alert('Deleted file!');
+        openForlderByPath(currentPath);
+    } else {
+        alert('Can\'t delete file');
+    }
+}
+
+function openForlder() {
+    openForlderByPath(currentPath + findFileName());
+}
+
+function findFileName() {
+    let box = menu.getBoundingClientRect();
+    let element = elementFromPoint(box.top + clientY, box.left + clientX);
+    return element.children[1].textContent;
+}
+
+async function openFolderByPath(path) {
+    let response = await fetch(serviceEndpoint + path)
+
+    if (response.ok) {
+        let jsonResponse = await response.json();
+
+        for (let i = 0; i < jsonResponse.length; i++) {
+            currentFiles.put({ fileName: jsonResponse[i]['name'], type: jsonResponse[i]['type'] });
+        }
+    } else {
+        alert('can\'t open folder.');
+    }
+}
+
+function showFiles() {
+    let table = document.querySelector('.table');
+
+    removeAllChild(table);
+
+    let lines = {};
+
+    for (let i = 0; i < currentFiles.length; i++) {
+        let line = document.createAttribute('tr');
+
+        // This sub cycle makes each line 9 elements length.
+        for (; i % 9 <= 9; i++) {
+            line.appendChild(formTagFromFile(currentFiles[i]));
+        }
+
+        lines.put(line);
+    }
+
+    lines.forEach(element => {
+        table.appendChild(element);
+    })
+}
+
+// TODO
+// add customize menu for each folder and file.
+
+function removeAllChild(tag) {
+    while (tag.firstChild) {
+        tag.removeChild(tag.firstChild);
+    }
+}
+
+function formTagFromFile(file) {
+    let element = document.createAttribute('button');
+    let elementImage = document.createAttribute('img');
+    let elementName = document.createAttribute('p');
+
+    if (currentFiles[i].type === 'folder') {
+        elementImage.setAttribute('src', 'folder.png');
+    } else {
+        elementImage.setAttribute('src', 'file.png');
+    }
+
+    elementName.textContent = currentFiles[i].fileName;
+    elementName.classList.add("element");
+
+    element.appendChild(elementImage);
+    element.appendChild(elementName);
+
+    return element;
+}
+
+/*
+JSON FORMAT
+
+{
+    {
+        'name': 'smth.txt',
+        'type': 'file'
+    },
+    {
+        'name': 'another.txt',
+        'type': 'file'
+    },
+    {
+        'name': 'asdf',
+        'type': 'folder'
+    }
+}
+*/
+
+async function createFile() {
+    let fileName = prompt('Enter file name', 'smth.txt');
+
+    let response = await fetch(serviceEndpoint + createFileEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ fileName: fileName, path: currentPath })
+    });
+
+    if (response.ok) {
+        alert('Created file!');
+        openForlderByPath(currentPath);
+    } else {
+        alert('can\'t create file');
     }
 }
